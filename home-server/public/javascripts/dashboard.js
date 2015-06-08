e5d4ede58933b673040d8eb5de57c68fec3b6e95
Max <max@froumentin.net>
2015-06-08 23:54:10 +0100

    put images with links on the svg

--- home-server/public/javascripts/dashboard.js
+++ home-server/public/javascripts/dashboard.js
@@ -1,24 +1,39 @@
-var $, d3;
+(function($, d3) {
 
-$(function() {
-  'use strict';
+  // from a date as 2015-05-30_21.22.29
+  // return a number from 0 to 1 depending on the time, midnight to midnight
+  function timeOfDay(dateString) {
+    var time = dateString.match(/_(\d{2})\.(\d{2})\.(\d{2})/);
+    return (time[1] * 3600 + time[2] * 60 + parseInt(time[3],10)) / 86400;
+  }
 
-  d3.json('/media', function(data) {
-    var day = d3.select('#content')
-      .selectAll('div')
-      .data(data)
-      .enter()
-      .append('div')
-    ;
 
-    day.append('h2').text(function(d) { return d.date; });
-    day.append('svg')
-      .selectAll('g')
-      .data(function(d) { return d.events; })
-      .enter()
-      .append('g')
-      .append('text').text(function(d) { return d.filePrefix; })
+  $(function() {
+    'use strict';
+
+    d3.json('/media', function(data) {
+      var day = d3.select('#content')
+        .selectAll('div')
+        .data(data)
+        .enter()
+        .append('div')
       ;
-  });
 
-});
+      day.append('h2').text(function(d) { return d.date; });
+      day.append('svg').attr('width', 1000).attr('height', 300)
+        .selectAll('g')
+        .data(function(d) { return d.events; })
+        .enter()
+        .append('a')
+          .attr('xlink:href', function(d) { return d.href + '.jpg'; })
+          .append('image')
+            .attr('xlink:href', function(d) { return d.href + '.jpg'; })
+            .attr('x', function(d) {
+              return timeOfDay(d.filePrefix) * 1000;
+            })
+            .attr('y', 0)
+            .attr('width', 50).attr('height', 50)
+      ;
+    });
+  });
+})($, d3);
