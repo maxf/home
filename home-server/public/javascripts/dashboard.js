ae50ea891515a1e2b317111cf2ceb58588dd979f
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2015-06-11 17:21:50 +0100

    display events as dots with tooltips

--- home-server/public/javascripts/dashboard.js
+++ home-server/public/javascripts/dashboard.js
@@ -1,17 +1,19 @@
 (function($, d3) {
+  'use strict';
 
   // from a date as 2015-05-30_21.22.29
   // return a number from 0 to 1 depending on the time, midnight to midnight
   function timeOfDay(dateString) {
     var time = dateString.match(/_(\d{2})\.(\d{2})\.(\d{2})/);
-    return (time[1] * 3600 + time[2] * 60 + parseInt(time[3],10)) / 86400;
+    return (time[1] * 3600 + time[2] * 60 + parseInt(time[3], 10)) / 86400;
   }
 
+  var hoursScale = d3.scale.linear().domain([0, 24]).range([0, 1000]);
 
   $(function() {
-    'use strict';
 
     d3.json('/media', function(data) {
+      var svg, timeAxis;
       var day = d3.select('#content')
         .selectAll('div')
         .data(data)
@@ -20,19 +22,45 @@
       ;
 
       day.append('h2').text(function(d) { return d.date; });
-      day.append('svg').attr('width', 1000).attr('height', 300)
-        .selectAll('g')
+      svg = day.append('svg').attr('width', 1000).attr('height', 50);
+      svg
+        .append('defs')
+          .append('symbol')
+            .attr('id', 'dot')
+            .append('circle')
+              .attr('cx', 5).attr('cy', 5).attr('r', 5)
+              .attr('style', 'stroke: none; fill: black')
+      ;
+
+      timeAxis = d3.svg.axis().scale(hoursScale).ticks(24);
+
+      svg.append('g')
+        .attr('transform', 'translate(0, 28)')
+        .call(timeAxis);
+
+      svg.selectAll('a')
         .data(function(d) { return d.events; })
         .enter()
         .append('a')
-          .attr('xlink:href', function(d) { return d.href + '.jpg'; })
-          .append('image')
-            .attr('xlink:href', function(d) { return d.href + '.jpg'; })
+          .attr('xlink:href', function(d) { return d.href + '.mp4'; })
+          .append('use')
+            .attr('xlink:href', '#dot')
             .attr('x', function(d) {
               return timeOfDay(d.filePrefix) * 1000;
             })
-            .attr('y', 0)
-            .attr('width', 50).attr('height', 50)
+            .attr('y', 10)
+            .on('mouseover', function(d) {
+              d3.select('#tooltip')
+                .style('top', d3.event.pageY + 20 + 'px')
+                .style('left', d3.event.pageX + 10 + 'px')
+                .style('display', 'block');
+              d3.select('#tooltip img')
+                .attr('src', d.href + '.jpg')
+              ;
+            })
+            .on('mouseout', function() {
+              d3.select('#tooltip').style('display', 'none');
+            })
       ;
     });
   });
