88acf2f824a640fc48dd8a54d821d8ba06d7cb4e
Max Froumentin <max@froumentin.net>
2015-09-22 21:17:54 +0100

    increase recording time and manage opening hours better

--- monitor-with-pir.py
+++ monitor-with-pir.py
@@ -6,14 +6,29 @@ import sys
 import os
 
 media_directory = 'home-server/public/images/media'
+
+period_1_start_time = datetime.time(9,15)
+period_1_end_time = datetime.time(17,30)
+period_2_start_time = datetime.time(0,0)
+period_2_end_time = datetime.time(7,15)
+
+
 def get_file_name():
     return media_directory + '/' + datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
 
+previous_in_hours = 4
 def in_hours():
+    global previous_in_hours
     now = datetime.datetime.now()
     weekday = now.weekday()
-    hour = now.hour
-    return weekday !== 5 and weekday !== 6 and hour > 9 and hour < 18
+    time = now.time();
+    in_hours = weekday != 5 and weekday != 6 and (period_1_start_time < time < period_1_end_time or period_2_start_time < time < period_2_end_time)
+    if (not in_hours) and (previous_in_hours == True):
+        print("exiting in-hours at " + str(time))
+    if in_hours and previous_in_hours == False:
+        print("entering in-hours at " + str(time))
+    previous_in_hours = in_hours
+    return in_hours
 
 sensor = 4
 
@@ -45,7 +60,7 @@ with picamera.PiCamera() as cam:
                     cam.start_recording(fileName + '.h264')
                     cam.wait_recording(10)
                     cam.capture(fileName+'.jpg', use_video_port=True)
-                    cam.wait_recording(20)
+                    cam.wait_recording(50)
                     cam.stop_recording()
                     print("stopped recording "+fileName)
                     print("converting to mp4")
@@ -53,5 +68,7 @@ with picamera.PiCamera() as cam:
                     os.remove(fileName + '.h264')
                     print("\a") # ring bell in terminal
         else:
-            time.sleep(60)
-            print(".")
+#            print("It's " + str(datetime.datetime.now()))
+#            print("out of hours")
+            time.sleep(300)
+
