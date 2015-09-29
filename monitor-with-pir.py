5ea9f64e88423e779a7d45b1d421adc463b7dc92
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2015-09-29 14:47:07 +0100

    read monitoring hours from separate file

--- monitor-with-pir.py
+++ monitor-with-pir.py
@@ -2,35 +2,46 @@ import RPi.GPIO as GPIO
 import time
 import picamera
 import datetime
+import json
 import sys
 import os
 
 media_directory = 'home-server/public/images/media'
+schedule = json.loads(open('schedule.json').read())
+previous_in_hours = -1
+sensor = 4
+previous_state = False
+current_state = False
 
-period_1_start_time = datetime.time(9,15)
-period_1_end_time = datetime.time(17,30)
-period_2_start_time = datetime.time(0,0)
-period_2_end_time = datetime.time(7,15)
+# returns True if the time passed is within one of the time ranges passed
+def in_time_ranges(the_time, time_ranges):
+    for this_range in time_ranges:
+        start_time = datetime.time(int(this_range[0][0:2]), int(this_range[0][3:5]))
+        end_time = datetime.time(int(this_range[1][0:2]), int(this_range[1][3:5]))
+        if start_time < the_time < end_time:
+            return True
+    return False
 
+# returns True if the day of the week passed is within on the day ranges passed
+def in_weekday_ranges(the_day, day_ranges):
+    for day_range in day_ranges:
+        if day_range[0] < the_day < day_range[1]:
+            return True
+    return False
+
+# returns True if the datetime passed is within the schedule hours
+def in_monitoring_hours(date_time, schedule):
+    time = date_time.time()
+    weekday = date_time.weekday()
+    for interval in schedule:
+        if interval['type'] == 'hours' and not in_time_ranges(time, interval['ranges']):
+                return False
+        if interval['type'] == 'weekdays' and not in_weekday_ranges(weekday, interval['ranges']):
+                return False
+    return True
 
 def get_file_name():
     return media_directory + '/' + datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
-
-previous_in_hours = 4
-def in_hours():
-    global previous_in_hours
-    now = datetime.datetime.now()
-    weekday = now.weekday()
-    time = now.time();
-    in_hours = weekday != 5 and weekday != 6 and (period_1_start_time < time < period_1_end_time or period_2_start_time < time < period_2_end_time)
-    if (not in_hours) and (previous_in_hours == True):
-        print("exiting in-hours at " + str(time))
-    if in_hours and previous_in_hours == False:
-        print("entering in-hours at " + str(time))
-    previous_in_hours = in_hours
-    return in_hours
-
-sensor = 4
 
 try:
     os.mkdir(media_directory)
@@ -40,14 +51,13 @@ except OSError:
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(sensor, GPIO.IN, GPIO.PUD_DOWN)
 
-previous_state = False
-current_state = False
 
 with picamera.PiCamera() as cam:
     cam.resolution = (800, 600)
     print("Starting at " + str(datetime.datetime.now()))
     while True:
-        if in_hours():
+        previous_in_hours = in_monitoring_hours(datetime.datetime.now, schedule)
+        if previous_in_hours:
             time.sleep(0.1)
             previous_state = current_state
             current_state = GPIO.input(sensor)
@@ -71,4 +81,3 @@ with picamera.PiCamera() as cam:
 #            print("It's " + str(datetime.datetime.now()))
 #            print("out of hours")
             time.sleep(300)
-
