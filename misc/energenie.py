253a9d8fbbb82c75d7bd8008d60cea3c64a6f1d8
Max <max@froumentin.net>
2015-12-06 16:55:55 +0000

    Add utilities

---
+++ misc/energenie.py
@@ -0,0 +1,13 @@
+from energenie import switch_on, switch_off
+from time import sleep
+
+# turn all plug sockets on and off
+switch_on()
+switch_off()
+
+# turn a plug socket on and off by number
+switch_on(1)
+switch_off(1)
+
+switch_on(2)
+switch_off(2)
