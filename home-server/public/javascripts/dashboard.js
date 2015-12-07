abbf8f9d5dee766339ce3efbc7070d01fcce915b
Max Froumentin <max@froumentin.net>
2015-12-07 11:00:04 +0000

    Control all switches with buttons

--- home-server/public/javascripts/dashboard.js
+++ home-server/public/javascripts/dashboard.js
@@ -1,12 +1,11 @@
 /*global $ */
 (function($) {
   'use strict';
-  $('#switch-on').on('click', function(event) {
-    $.post('api/on');
-  });
-
-  $('#switch-off').on('click', function(event) {
-    $.post('api/off');
-  });
+  $('#switch-0-on').on('click', function(event) { $.post('api/0/on'); });
+  $('#switch-0-off').on('click', function(event) { $.post('api/0/off'); });
+  $('#switch-1-on').on('click', function(event) { $.post('api/1/on'); });
+  $('#switch-1-off').on('click', function(event) { $.post('api/1/off'); });
+  $('#switch-2-on').on('click', function(event) { $.post('api/2/on'); });
+  $('#switch-2-off').on('click', function(event) { $.post('api/2/off'); });
 
 })($);
--- home-server/routes/api.js
+++ home-server/routes/api.js
@@ -5,16 +5,18 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-router.post('/on', function(req, res, next) {
-  request(req.app.locals.socketsApiUrl + 'on', function (error, response, body) {
-    res.json({success: !error && response.statusCode == 200});
-  });
+router.post('/:id/on', function(req, res, next) {
+  request(req.app.locals.socketsApiUrl + req.params.id + '/on',
+          function (error, response, body) {
+            res.json({success: !error && response.statusCode == 200});
+          });
 });
 
-router.post('/off', function(req, res, next) {
-  request(req.app.locals.socketsApiUrl + 'off', function (error, response, body) {
-    res.json({success: !error && response.statusCode == 200});
-  });
+router.post('/:id/off', function(req, res, next) {
+  request(req.app.locals.socketsApiUrl + req.params.id + '/off',
+   function (error, response, body) {
+     res.json({success: !error && response.statusCode == 200});
+   });
 });
 
 module.exports = router;
