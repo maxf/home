53ed40d7e16a4d56ab0e4cfe950432bf92cf9559
Max Froumentin <max@froumentin.net>
2015-12-07 23:31:04 +0000

    Use jquery-ui sliders for time ranges

--- home-server/public/javascripts/dashboard.js
+++ home-server/public/javascripts/dashboard.js
@@ -1,6 +1,17 @@
 /*global $ */
 (function($) {
   'use strict';
+
+  var sliderOptions = {
+    max: 24,
+    min: 0,
+    range: true,
+    values: [8, 18],
+    change: function(event, ui) { console.log(event.target, ui.values); }
+  }
+
+  $('.slider').slider(sliderOptions);
+
   $('#switch-0-on').on('click', function(event) { $.post('api/0/on'); });
   $('#switch-0-off').on('click', function(event) { $.post('api/0/off'); });
   $('#switch-1-on').on('click', function(event) { $.post('api/1/on'); });
