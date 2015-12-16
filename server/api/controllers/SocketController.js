09b80005453a4a42eb46fe12de9878a837e9faa1
Max Froumentin <max@froumentin.net>
2015-12-16 21:55:16 +0000

    Redirect after POST requests to prevent reload resends

--- server/api/controllers/SocketController.js
+++ server/api/controllers/SocketController.js
@@ -29,37 +29,31 @@ module.exports = {
 
   socketsPage: function(req, res) {
 
-    function sendView(res) {
-     SocketService.getSocketsForView(function(socketsForView) {
-       res.view('sockets', {
-         sockets: socketsForView
-       });
-     });
-    }
-
     if (req.route.method === 'post') {
-      // Either an existing socket was modified, or a new one was created
       if (req.body.id) {
         SocketService.modifySocketFromView(req.body, function(success) {
           if (!success) throw 'Failed to modify socket';
-          sendView(res);
+          res.redirect('/sockets/')
         });
       } else {
         if (req.body.delete_id) {
           SocketService.removeSocket(req.body.delete_id, function(success) {
             if (!success) throw 'Failed to delete socket';
-            sendView(res);
+            res.redirect('/sockets/')
           });
         } else {
-          // Create a new socket
           SocketService.addSocketFromView(req.body, function(success) {
             if (!success) throw 'Failed to add socket';
-            sendView(res);
+            res.redirect('/sockets/')
           });
         }
       }
     } else {
-      sendView(res);
+      SocketService.getSocketsForView(function(socketsForView) {
+        res.view('sockets', {
+          sockets: socketsForView
+        });
+      });
     }
   }
 };
