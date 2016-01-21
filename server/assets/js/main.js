c976c50df9ca131a2e1d0744256112cd53d2ec71
Max <max@froumentin.net>
2016-01-21 23:56:59 +0000

    Change timer mode without submitting form

--- server/assets/js/main.js
+++ server/assets/js/main.js
@@ -1,1 +1,36 @@
-console.log('ok');
+$(function() {
+
+  $('.socket input[name=timerMode]').on('change', timerModeChange);
+
+
+  function timerModeChange(event) {
+    var $socket = $(event.target).parents('.socket');
+    var $timerCtrl = $socket.find('.timer');
+    var $onOffCtrl = $socket.find('.switchedOn');
+    var socketId = $socket.find('form input[name=id]').val();
+
+    if (event.target.value === 'on') {
+      $.ajax({
+	url: '/socket/' + socketId,
+	type: 'PUT',
+	contentType: 'application/json',
+	data: '{ "timerMode": true }'
+      }).success(function() {
+        $timerCtrl.show();
+        $onOffCtrl.hide();
+      });
+    } else {
+      $.ajax({
+	url: '/socket/' + socketId,
+	type: 'PUT',
+	contentType: 'application/json',
+	data: '{ "timerMode": false }'
+      }).success(function() {
+        $timerCtrl.hide();
+        $onOffCtrl.show();
+      });
+    }
+  };
+
+
+});
