1e1fea992cdd1fe2faccc5e37184caac003544a9
Max <max@froumentin.net>
2016-04-09 12:14:39 +0100

    Improvements to diff-based motion detection

--- monitor.py
+++ monitor.py
@@ -1,13 +1,15 @@
 import subprocess as sub
 import os
 from datetime import datetime
+import time
 
 imagename="image"
+image_dir='server/assets/images/'
 diff_threshold=30
-debug=1
+verbosity=2
 
-def log(message):
-  if (debug):
+def log(message, importance):
+  if (verbosity <= importance):
     print(message)
 
 def go(iso_datetime):
@@ -16,25 +18,27 @@ def go(iso_datetime):
   except OSError:
     pass
   take_picture()
-#  log("checking diff")
+  log("checking diff", 1)
   diff = sub.check_output(['compare', '-metric', 'PSNR', imagename+'.jpg',
                            imagename+'-prev.jpg', 'diff.jpg'],
                            stderr = sub.STDOUT)
-#  log("diff: "+diff)
+  log("diff: "+diff, 1)
   if float(diff) < diff_threshold:
-    log("Alert! "+diff+" at "+iso_datetime)
-    sub.call(['cp', imagename+'.jpg', imagename+iso_datetime+'---'+str(float(diff))+'.jpg'])
+    log("Alert! "+diff+" at "+iso_datetime, 2)
+    sub.call(['cp', imagename+'.jpg', image_dir+imagename+iso_datetime+'---'+str(float(diff))+'.jpg'])
 
 def take_picture():
-#  log("taking picture")
-  sub.call(['raspistill', '-o', imagename+'.jpg'])
+  log("taking picture", 1)
+  sub.call(['raspistill', '-md', '4', '-t', '1', '-n', '-o', imagename+'.jpg'])
+  sub.call(['cp', imagename+'.jpg', image_dir+imagename+'.jpg'])
 
 
-log('= Start ======= ' + str(datetime.now()))
+log('= Start ======= ' + str(datetime.now()), 2)
 
 while 1==1:
   iso_datetime = datetime.now().isoformat()
 
   take_picture()
-#  log('======== ' + iso_datetime)
+  time.sleep(1)
+  log('======== ' + iso_datetime, 1)
   go(iso_datetime)
