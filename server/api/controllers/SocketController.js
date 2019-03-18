daefd4cf7b2e142e52bad1fe28f978febfebf5d1
Max Froumentin <github@e97.me>
2019-03-18 19:09:15 +0000

    improved addressing of switches

--- server/api/controllers/SocketController.js
+++ server/api/controllers/SocketController.js
@@ -11,30 +11,36 @@ const defaultAdd = require(defaultActionsPath+'add.js'); // PUT
 const defaultCreate = require(defaultActionsPath+'create.js'); // POST
 const defaultReplace = require(defaultActionsPath+'replace.js'); // PUT
 const defaultUpdate = require(defaultActionsPath+'update.js'); // PATCH
+const defaultDestroy = require(defaultActionsPath+'destroy.js'); // DELETE
 const request = require('request-promise-native');
 
 
+const updateSwitch = async function(id, state) {
+  const onOrOff = state === 'true' ? 'on' : 'off';
+  const apiUrl = `http://localhost:5000/set_switch/${id}/${onOrOff}`;
+  console.log(`sending "${onOrOff}" to switch ${id}`);
+  try {
+    await request.get(apiUrl);
+  } catch (e) {
+    console.log(`error, the socket API returned an error: ${e}`);
+  }
+};
+
 module.exports = {
   add: async function (req, res) {
-    try {
-      await request.get('http://localhost:5000/set_switch/0/on');
-    } catch (e) {
-      console.log(`error, the socket API returned an error: ${e}`);
-    }
     return defaultAdd(req, res);
   },
   create: async function (req, res) {
     return defaultCreate(req, res);
   },
   update: async function (req, res) {
-    try {
-      await request.get('http://localhost:5000/set_switch/0/on');
-    } catch (e) {
-      console.log(`error, the socket API returned an error: ${e}`);
-    }
+    updateSwitch(req.body.physicalSocket, req.body.switchedOn);
     return defaultUpdate(req, res);
   },
   replace: async function (req, res) {
     return defaultReplace(req, res);
+  },
+  destroy: async function (req, res) {
+    return defaultDestroy(req, res);
   }
 };
